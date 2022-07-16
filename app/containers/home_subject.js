224b27f427a428c8b33f9bdf3fafcc107ef54669
Elkin Burbano <belkin@unicauca.edu.co>
2022-07-16 12:35:16 -0500

    [BUG] other try to fix local permisions

--- app/containers/home_subject.js
+++ app/containers/home_subject.js
@@ -1,57 +1,64 @@
-import React,{Component, Fragment} from 'react';
-import Header from '../components/header';
-import SuggestionList from '../containers/subject-list';
-import { StyleSheet} from 'react-native';
-import API from '../../utils/api';
-import {connect} from 'react-redux';
+import React, { Component, Fragment } from "react";
+import Header from "../components/header";
+import SuggestionList from "../containers/subject-list";
+import { StyleSheet } from "react-native";
+import API from "../../utils/api";
+import { connect } from "react-redux";
 
-class Home extends Component{
-  static navigationOptions =({navigation})=>{
-    return{
-      header: <Header onPress={()=>navigation.openDrawer()}>Mis Cursos</Header>,
-    }
+class Home extends Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      header: (
+        <Header onPress={() => navigation.openDrawer()}>Mis Cursos</Header>
+      ),
+    };
+  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      subject: [],
+    };
   }
-    constructor(props){
-        super(props);
-        this.state ={
-          loading:false,
-          subject: [],
-        }
-      }
-      async componentDidMount(){
-        console.log(this.props.ipconfig);
-        console.log(this.props.student);
-        const subject = await API.getCourses(this.props.ipconfig, this.props.student.grado_estudiante, this.props.student.id_colegio);
-        this.props.dispatch({
-          type:'SET_ACTIVITIES_LIST',
-          payload:{
-            subject
-          }
-        })
-      }
-    render(){
-        return(
-            <Fragment>
-                <SuggestionList></SuggestionList>
-            </Fragment>
-        );
-    }
+
+  async componentDidMount() {
+    console.log(this.props.ipconfig);
+    console.log(this.props.student);
+    const subject = await API.getCourses(
+      this.props.ipconfig,
+      this.props.student.grado_estudiante,
+      this.props.student.id_colegio
+    );
+    this.props.dispatch({
+      type: "SET_ACTIVITIES_LIST",
+      payload: {
+        subject,
+      },
+    });
+  }
+  render() {
+    return (
+      <Fragment>
+        <SuggestionList></SuggestionList>
+      </Fragment>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  texto:{
-    color:'white',
+  texto: {
+    color: "white",
     fontSize: 17,
-    fontWeight:"bold",
+    fontWeight: "bold",
     marginLeft: 20,
-  }
-})
+  },
+});
 
-function mapStateToProps(state){
-  return{
-      ipconfig: state.videos.selectedIPConfig,
-      student:state.videos.selectedStudent
-  }
+function mapStateToProps(state) {
+  return {
+    ipconfig: state.videos.selectedIPConfig,
+    student: state.videos.selectedStudent,
+  };
 }
 
-export default connect(mapStateToProps) (Home);
+export default connect(mapStateToProps)(Home);
