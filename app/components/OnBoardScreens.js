d8ad706b3b7caf7a66e51211921c6269941ad1b2
Santiago Burbano <santiago.burbano@itconsultores.com.co>
2022-10-04 17:24:23 -0500

    [FEATURE] OnboardScreen some styling

--- app/components/OnBoardScreens.js
+++ app/components/OnBoardScreens.js
@@ -1,7 +1,8 @@
-import { View, Text, Image } from "react-native";
+import { View, Image } from "react-native";
 import React from "react";
 import Onboarding from "react-native-onboarding-swiper";
 import saludo from "../../assets/images/saludo.png";
+import robotPregunta from "../../assets/images/robotPregunta.png";
 import { Dimensions } from "react-native";
 
 const OnBoardScreens = ({ setFirstLaunch }) => {
@@ -16,28 +17,33 @@ const OnBoardScreens = ({ setFirstLaunch }) => {
         pages={[
           {
             backgroundColor: "#F5F5F5",
-            image: <Image source={saludo} />,
+            image: (
+              <Image source={saludo} style={{ width: 300, height: 390 }} />
+            ),
             title: "Bienvenido",
-            subtitle: "Este es un tutorial de como usar SmartFC",
+            subtitle:
+              "Este es un pequeño tutorial que te enseñará a usar SmartFC.",
           },
           {
             backgroundColor: "#F5F5F5",
-            image: <Image source={saludo} />,
+            image: <Image source={robotPregunta} />,
             title: "Primer Paso",
             subtitle:
-              "Debes primero hacer la conexión de la Ip en conectar tu IP ",
+              "Debes primero hacer la conexión de la Ip en conectar tu IP. ",
           },
           {
             backgroundColor: "#F5F5F5",
-            image: <Image source={saludo} />,
+            image: (
+              <Image source={saludo} style={{ width: 300, height: 390 }} />
+            ),
             title: "Segundo Paso",
             subtitle:
-              "Debes registrar tu usuario en la aplicación, en el apartado de Registrate",
+              "Debes registrar tu usuario en la aplicación, en el apartado de Regístrate.",
           },
           {
             backgroundColor: "#F5F5F5",
-            image: <Image source={saludo} />,
-            title: "Tercer Paso",
+            image: <Image source={robotPregunta} style={{ marginTop: 20 }} />,
+            title: "Último Paso",
             subtitle:
               "El siguiente paso es presionar el botón Sincronizar datos de usuario y ya puedes iniciar sesión con tu usuario registrado!",
           },
@@ -45,6 +51,10 @@ const OnBoardScreens = ({ setFirstLaunch }) => {
         onDone={() => {
           setFirstLaunch(false);
         }}
+        showSkip={false}
+        bottomBarColor={"#F5F5F5"}
+        titleStyles={{ color: "#70C2E5", fontSize: 33 }}
+        subTitleStyles={{ color: "#424B5B", fontSize: 20 }}
       />
     </View>
   );
